251ef9414b1c52c0626a21b8de8e39182ba719cb
Panupong Tipjoi <panupongza_155@hotmail.com>
2020-01-31 05:15:44 +0700

    Chang title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Nav from '../components/nav'
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
+      <title>Login and Logout</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
