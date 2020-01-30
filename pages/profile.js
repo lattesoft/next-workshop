411a81c17e51ec32d2867566911e2573caa78e53
Panupong Tipjoi <panupongza_155@hotmail.com>
2020-01-31 06:34:51 +0700

    Workshop6-problem

--- pages/profile.js
+++ pages/profile.js
@@ -1,29 +1,12 @@
 import Layout from "../components/layout";
 import Logout from "../components/logout";
-import cookie from 'js-cookie';
-import { useRouter } from 'next/router';
-import Link from "next/link";
 
 const Profile = () => {
-  const router = useRouter();
-  const getToken = cookie.get('token');
-
   return(
     <Layout>
       <div className="center-text">
-        {getToken ? (
-          <>
-            <h2>PROFILE</h2>
-            <Logout />
-          </>
-        ) : (
-          <>
-            <h2>Not Login</h2>
-            <Link href="/login">
-              <a>Back to Login</a>
-            </Link>
-          </>
-        )}
+        <h2>PROFILE</h2>
+        <Logout />
       </div>
     </Layout>
   )
